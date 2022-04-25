45e9e34d97a23bbd25ecf790b8ae25abfe374ca9
emanwiens <emanwiens@gmail.com>
2022-04-25 09:56:54 -0500

    Added update script

---
+++ update.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "sudo apt-get update"
+sudo apt-get update
+echo "sudo apt update"
+sudo apt update 
+
+echo "sudp apt-get upgrade"
+sudo apt-get upgrade 
+echo "sudo apt upgrade"
+sudo apt upgrade
+
